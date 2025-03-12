c5202ac0aa8b90e13cb3f679fbfd95e2cf3da0dd
Englis666 <eabarros2610@gmail.com>
2025-03-11 21:55:01 -0500

    Feature: <CreateTask> : vista de formulario de tareas

---
+++ frontend/src/pages/CreateTask.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import FormCreateTask from "../components/FormCreateTask";
+
+export default function CreateTask() {
+    return (
+        <Box>
+            <FormCreateTask />
+        </Box>
+
+    );
+
+}
